66778a25660c8050d9d455ca027f9ceb3e97b763
Mouna <mounaouriagli2@gmail.com>
2023-04-03 16:22:56 -0700

    more_even_pointers_tasks

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,12 +9,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i,j;
+	char *p = dest;
 
 	i = n;
 
-	for (j = 0; j < i; j++)
+	for (j = 0; j < n; j++)
 	{
-		dest[j] = src[j];
+		*p = *src;
 		n--;
 	}
 	return (dest);
