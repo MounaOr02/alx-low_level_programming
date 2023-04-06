c75cfe43ff46e6a369413cb8043b2358c07e227f
Mouna <mounaouriagli2@gmail.com>
2023-04-05 18:40:52 -0700

    recursion_tasks

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-  *factorial - calculate the fuctorial of an integer
-  *@n: the integer
-  *Return: factorial of n or -1 if n is negative
-  */
+ * factorial - calculate the fuctorial of an integer
+ * @n: the integer
+ * Return: factorial of n or -1 if n is negative
+ */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (1);
-	return (n * factoriel(n - 1));
+	return (n * factorial(n - 1));
 }
