f92ba66507b62a6764e83060ac80c2125eb8c4ef
Mouna <mounaouriagli2@gmail.com>
2023-04-17 19:56:50 -0700

    0x0E-structures_typedef_tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,7 @@ void print_dog(struct dog *d);
  * dog_t - Outside of a dog, a book is a man's best friend.
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
